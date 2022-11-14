601c7db100acfc2b7588bc993986d8e0db347626
Shristi <shristigautam90@gmail.com>
2022-11-13 22:15:02 -0600

    Initial commit

---
+++ app/src/main/java/com/shristi/shristi_resume/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shristi.shristi_resume
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
